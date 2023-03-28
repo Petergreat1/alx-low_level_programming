ab568f332affb00a7ca269c4428a15e7981ca396
Peter <toby4jms@gmail.com>
2023-03-28 12:27:36 -0700

    8-print_array.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,8 @@
+#include "main.h"
+/**
+ * print_array - a function that prints n elements of array
+ * @a: array name
+ * @n: is the number of elements of the array to be printed
+ * Return: a and n inputs
+ */
+
