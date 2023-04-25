72b3695b9e203b79cfd8f7b23ae92477bf6a39e4
Peter <toby4jms@gmail.com>
2023-04-25 15:35:03 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,36 +1,30 @@
 #include "main.h"
-
 /**
- * cap_string - function that capitalizes all words of a string.
- * @s: pointer string.
- *
- * Return: pointer to s.
+ * cap_string - capitalizes all words of a string
+ * @s: input string.
+ * Return: the pointer to dest.
  */
+
 char *cap_string(char *s)
 {
-	int string_count;
+	int count = 0, i;
+	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-/* scan through string */
-	string_count = 0;
-	while (s[string_count] != '\0')
+	if (*(s + count) >= 97 && *(s + count) <= 122)
+		*(s + count) = *(s + count) - 32;
+	count++;
+	while (*(s + count) != '\0')
 	{
-		/* if next character after count is a char , capitalise it */
-		if (s[0] >= 97 && s[0] <= 122)
+		for (i = 0; i < 13; i++)
 		{
-			s[0] = s[0] - 32;
+			if (*(s + count) == sep_words[i])
+			{
+				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
+					*(s + (count + 1)) = *(s + (count + 1)) - 32;
+				break;
+			}
 		}
-		if (s[string_count] == ' ' || s[string_count] == '\t' || s[string_count] == '\n'
-				|| s[string_count] == ',' || s[string_count] == ';' || s[string_count] == ','
-				|| s[string_count] == ',' || s[string_count] == '!' || s[string_count] == '?'
-				|| s[string_count] == '"' || s[string_count] == '(' || s[string_count] == ')'
-				|| s[string_count] == '{' || s[string_count] == '}')
-		{
-			if (s[string_count + 1] >= 97 && s[string_count + 1] <= 122)
-					{
-					s[string_count + 1] = s[string_count + 1] - 32;
-					}
-					}
-					string_count++;
-					}
-					return (s);
-					}
+		count++;
+	}
+	return (s);
+}
